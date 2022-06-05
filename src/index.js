2b249d3f10c837bf29b76e9013fd796f5b362496
Snehal <snehalkhandelwal13@gmail.com>
2022-06-05 15:43:33 +0530

    mongo-Module

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh87698-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://snehal_3497:snehal_3497@atlascluster.q9xoryr.mongodb.net/snehal3497-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
---
+++ src/models/bookModel.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema( {
+    bookName:  {
+        type: String,
+        unique: true,
+        required: true
+    },
+    authorName: String,
+    catogory : String,
+    year : Number,
+
+}, { timestamps: true });
+
+module.exports = mongoose.model('Book', bookSchema)
+
+
