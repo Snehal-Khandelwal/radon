ce24ddac459fcc274cd2d2d39aa6319283e5dfa4
Snehal <snehalkhandelwal13@gmail.com>
2022-06-06 13:32:52 +0530

    save

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://snehal_3497:snehal_3497@atlascluster.q9xoryr.mongodb.net/snehal3497-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://vikas:1234567890@cluster0.mkjn9xp.mongodb.net/vikas555-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
